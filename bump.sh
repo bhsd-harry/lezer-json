1a661cb510253e3d3dbb86c8ec73b8a6eddd3ae4
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2026-06-02 17:04:15 +0800

    fix(bump.sh): prune git tags

--- bump.sh
+++ bump.sh
@@ -3,6 +3,7 @@ gsed -i -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
 npm run lint && npm run build
 if [[ $? -eq 0 ]]
 then
+	git fetch --prune --prune-tags
 	git add -A
 	git commit -m "chore: bump version to $1"
 	git push
